b9a68d6f733331505e55864feae45911ee885c5a
github_username <you@example.com>
2023-04-14 10:38:40 +0700

    main

---
+++ canvas.js
@@ -0,0 +1,12 @@
+// (function () {
+//     function playMusic() {
+//         var myAudio = document.getElementById('playAudio');
+//         myAudio.play();
+//     }
+
+//     body.addEventListener("click", function (ev) {
+//         playMusic()
+//     })
+// });
+
+document.getElementById('playAudio').play();
